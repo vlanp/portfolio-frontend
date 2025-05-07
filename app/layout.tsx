cfec62dc3fa2309c37dd72bcd01f6c00ee2f953b
Valentin <guillaumevalentin76@gmail.com>
2025-05-07 16:22:36 +0200

    Resolving issue with favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-providers";
+import Favicon from "./favicon.ico";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Valentin GUILLAUME Portfolio",
   description: "",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
