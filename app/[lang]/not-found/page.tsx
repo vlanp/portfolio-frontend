1579e6ba0b84770b09fae95f1d61c0c72d5b3ce8
Valentin <guillaumevalentin76@gmail.com>
2025-06-03 00:39:35 +0200

    Improving 404 not found page

--- app/[lang]/not-found/page.tsx
+++ app/[lang]/not-found/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { HiOutlineFaceFrown } from "react-icons/hi2";
 import { getDictionary, IDictionary } from "../dictionaries";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 
 export default async function NotFound({
   params,
@@ -11,13 +11,15 @@ export default async function NotFound({
   const { lang } = await params;
   const dict: IDictionary = await getDictionary(lang);
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center gap-2">
+    <main className="flex flex-1 w-screen flex-col items-center justify-center gap-2">
       <HiOutlineFaceFrown className="text-gray-400 size-10" />
       <h2 className="text-xl font-semibold">404 Not Found</h2>
       <p>{dict["404NotFound"].Description}</p>
-      <Link className={buttonVariants({ variant: "outline" })} href="/">
-        {dict["404NotFound"].GoBack}
-      </Link>
+      <Button asChild variant={"outline"}>
+        <Link className={buttonVariants({ variant: "outline" })} href="/">
+          {dict["404NotFound"].GoBack}
+        </Link>
+      </Button>
     </main>
   );
 }
