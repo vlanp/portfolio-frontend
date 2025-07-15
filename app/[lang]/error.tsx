bcc9073bdeb162aaa2ce8df1219babac01e3ab98
Valentin <guillaumevalentin76@gmail.com>
2025-07-15 17:22:57 +0200

    Adding a temporary toast to show errors

--- app/[lang]/error.tsx
+++ app/[lang]/error.tsx
@@ -5,6 +5,7 @@ import { startTransition, useEffect } from "react";
 import coyoteFail from "../../public/coyote-fail.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Error({
   error,
@@ -17,6 +18,7 @@ export default function Error({
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
+    toast.error(error.message);
   }, [error]);
 
   const handleClick = () => {
